613b240b5545c275d1e0b7406398c40e9b78b561
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2020-07-02 21:49:55 -0400

    Added basic home page with single product page

---
+++ pages/products/_id/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div v-if="this.product !== null">
+      {{this.product.Name}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      product: null
+    }
+  },
+  created: async function () {
+    const res = await fetch(`http://localhost:1337/products/${this.$route.params.id}`)
+    this.product = await res.json()
+  }
+}
+</script>
+
+<style>
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
