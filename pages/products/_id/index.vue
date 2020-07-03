e271f80d779554f46b01da3fd5f02089838b6027
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2020-07-02 23:19:23 -0400

    Added snipcart + better product pages

--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="container">
     <div v-if="this.product !== null">
-      {{this.product.Name}}
+      <button class="snipcart-add-item"
+        :data-item-id="product.id"
+        :data-item-price="product.Price"
+        :data-item-url="`${this.$route.fullPath}`"
+        :data-item-description="product.Description"
+        :data-item-image="`http://localhost:1337${product.Image[0].url}`"
+        :data-item-name="product.Name"
+        v-bind="customFields">
+        Add to cart
+      </button>
     </div>
   </div>
 </template>
@@ -13,6 +22,14 @@ export default {
       product: null
     }
   },
+  computed: {
+    customFields(){
+      return this.product["Custom_field"]
+        .map((field, index) => Object.entries(field)
+          .map(([key, value]) => ({[`data-item-custom${index + 1}-${key.toString().toLowerCase()}`]: value})))
+        .reduce((acc, curr) => acc.concat(curr), [])
+    }
+  },
   created: async function () {
     const res = await fetch(`http://localhost:1337/products/${this.$route.params.id}`)
     this.product = await res.json()
@@ -21,13 +38,4 @@ export default {
 </script>
 
 <style>
-
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 </style>
