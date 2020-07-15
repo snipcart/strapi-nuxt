b2ffe11550106fdb19100940767a13c91206b1d0
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2020-07-15 18:28:05 -0400

    Update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,9 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel='stylesheet', href="https://cdn.snipcart.com/themes/v3.0.16/default/snipcart.css" },
+      { rel: 'preconnect', href="https://app.snipcart.com" },
+      { rel: 'preconnect', href="https://cdn.snipcart.com" },
+      { rel: 'stylesheet', href="https://cdn.snipcart.com/themes/v3.0.16/default/snipcart.css" },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
