53f4379773160ac259e8bda097e3da5a723aa5df
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2020-07-15 17:59:32 -0400

    Updated imgs

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div v-for="p in products" :key="p.id" class="w-1/3 mt-6 ml-6 mr-6 border rounded-lg">
       <nuxt-link :to="`/products/${p.id}`">
         <div :style="{backgroundImage: p.gradient}">
-            <img class="mx-auto" :src="`https://strapi-snipcart.herokuapp.com${p.image[0].formats.thumbnail.url}`">
+            <img class="crop mx-auto" :src="p.image">
         </div>          
         <div class="ml-4 mt-4 mb-4 mr-4">
           <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{p.title}}</h4>
@@ -29,47 +29,8 @@ export default {
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.crop {
+  width: 180px;
+  height: 180px;
 }
 </style>
--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -3,7 +3,7 @@
       <div class="w-2/3 mt-6 ml-6 mr-6 border rounded-lg" v-if="this.product !== null">
         <div>
             <div :style="{backgroundImage: product.gradient}">
-            <img class="mx-auto" :src="`https://strapi-snipcart.herokuapp.com${product.image[0].formats.small.url}`">
+            <img class="mx-auto" :src="product.image" width="500">
         </div>          
         <div class="ml-4 mt-4 mb-4 mr-4">
           <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{product.title}}</h4>
@@ -15,7 +15,7 @@
             :data-item-price="product.price"
             :data-item-url="`${this.$route.fullPath}`"
             :data-item-description="product.description"
-            :data-item-image="`https://strapi-snipcart.herokuapp.com${product.image[0].url}`"
+            :data-item-image="product.image"
             :data-item-name="product.title"
             v-bind="customFields">
             Add to cart
